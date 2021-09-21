8d0976d808bebce9e506bbe115d2c2fa5c82275c
Bozhena Luskavets <luskavetsbozhena@gmail.com>
2021-09-21 18:52:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+
+// let txt = "";
+//     for (let i = 0; i < 10; i++) {
+//     txt += "*";
+//     console.log(txt);
+// }
+
+
+
+// **********
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+// n = 10;
+// let result = "";
+// for (let i = 0; i < n; i++) {
+//   for (let k = 0; k < n - i; k++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+
+
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+// *  *  *  *  *  *  *  *  *  *  
+
+// let count = 10;
+// let symbol = '*  ';
+// let result = '';
+
+// for (let i = 0; i < count; i++) {
+//     result = result + symbol;
+// }
+
+// for (let i = 0; i < count; i++) {
+//     console.log(result);
+// }
+
+
+
+// *  *  *  *  *  *  *  *  *  *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *                          *  
+// *  *  *  *  *  *  *  *  *  *  
+
+// let count = 10;
+// let symbol = '*  ';
+// let space = "   ";
+// let result = '';
+
+// for (let i = 0; i < count; i++) {
+//     for (let j = 0; j < count; j++) {
+//         if (i === 0 || i === count - 1) {
+//             result = result + symbol;
+//         } else {
+//             if (j === 0 || j === count - 1) {
+//                 result = result + symbol;
+//             } else {
+//                 result = result + space;
+//             }
+//         }
+//     }
+//     console.log(result);
+//     result = '';
+// }
